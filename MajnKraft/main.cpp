1f0189af04fa22837634c89ef79e1505c03c8441
matus <matus@matus>
2014-10-29 12:52:39 +0000

    sdb

--- MajnKraft/main.cpp
+++ MajnKraft/main.cpp
@@ -5,14 +5,10 @@
  * Created on October 29, 2014, 12:23 PM
  */
 
-#include <cstdlib>
 #include <iostream>
 
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
     cout << "Nazdar!!!" << endl;
     return 0;
