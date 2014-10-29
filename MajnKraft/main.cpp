4f0bd26f3043f7dcb314267062380c7ba8403512
matus <matus@matus>
2014-10-29 12:48:10 +0000

    sdb

--- MajnKraft/main.cpp
+++ MajnKraft/main.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
@@ -13,7 +14,7 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-
+    cout << "Nazdar!!!" << endl;
     return 0;
 }
 
