e664d598bce4cd90e8e58939520853d3da05af1a
Robert Hudec <nisanick@gmail.com>
2014-11-05 10:44:54 +0100

    initial project commit

--- MajnKraft/main.cpp
+++ MajnKraft/main.cpp
@@ -6,14 +6,30 @@
  */
 
 #include <SDL2/SDL.h>
-
-using namespace std;
+#include <SDL2/SDL_opengl.h>
 
 int main(int argc, char** argv) {
-    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Init(SDL_INIT_VIDEO);
+    
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    
+    SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    
+    SDL_Event windowEvent;
+    while (true)
+    {
+        if (SDL_PollEvent(&windowEvent))
+        {
+            if (windowEvent.type == SDL_QUIT) break;
+        }
 
-    SDL_Delay(1000);
-
+        SDL_GL_SwapWindow(window);
+    }
+    
+    SDL_GL_DeleteContext(context);
     SDL_Quit();
     return 0;
 }
