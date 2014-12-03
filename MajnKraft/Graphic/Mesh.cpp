1662d89637c727674e00fc597e52b368a9b8b9d5
Lukas <Lukas@172.20.52.13>
2014-12-03 13:27:06 +0100

    Created Graphic folder with graphic related content

--- MajnKraft/Graphic/Mesh.cpp
+++ MajnKraft/Graphic/Mesh.cpp
@@ -10,13 +10,13 @@ Mesh::Mesh() {
 
 void Mesh::loadCube() {
 // Create Vertex Array Object
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+    glGenVertexArrays(1, &vao); //vytvori odkaz(int) na pole vertexov
+    glBindVertexArray(vao); //prikaze pouzivat zadane pole  vertexov
 
     // Create a Vertex Buffer Object and copy the vertex data to it
     
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glGenBuffers(1, &vbo);  //vytvori buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vbo); //urci typ bufera a prikaze pouzivat zadany  buffer
 
     GLfloat vertices[] = {
         -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
@@ -61,14 +61,14 @@ void Mesh::loadCube() {
         -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
         -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
     };
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //nacita body do buffra
 }
 
 void Mesh::draw(GLuint shaderProgram) {
 // Specify the layout of the vertex data
-    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-    glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");       //nacita z shaderu poziciu premmennej position
+    glEnableVertexAttribArray(posAttrib);                                   //umozni vyuzitie posAttrib pri vykreslovani glDrawArrays or glDrawElements.
+    glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);    //specifies the location and data format of the array of generic vertex attributes at index index to use when rendering
 
     GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
     glEnableVertexAttribArray(colAttrib);
@@ -79,17 +79,17 @@ void Mesh::draw(GLuint shaderProgram) {
     glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
 
     GLint uniColor = glGetUniformLocation(shaderProgram, "overrideColor");
-    glUniform3f(uniColor, 1.0f, 1.0f, 1.0f);
+    glUniform3f(uniColor, 1.0f, 1.0f, 1.0f);    //nastavi v shadery uniColor na danu  hodnotu
     
     glDrawArrays(GL_TRIANGLES, 0, 36);
     
-    glDisableVertexAttribArray(posAttrib);
+    glDisableVertexAttribArray(posAttrib); //opak EnableVertexArray
     glDisableVertexAttribArray(colAttrib);
     glDisableVertexAttribArray(texAttrib);
     glDisableVertexAttribArray(uniColor);
 }
 
 Mesh::~Mesh() {
-    glDeleteBuffers(1, &vbo);
-    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);           //uvolni buffer
+    glDeleteVertexArrays(1, &vao);      //uvolni pole vertexov
 }
--- MajnKraft/main.cpp
+++ MajnKraft/main.cpp
@@ -9,10 +9,10 @@
 
 
 #include "SDL2/SDL.h"
-#include "Graphic\Mesh.h"
-#include "Graphic\Shader.h"
-#include "Graphic\Texture.h"
-#include "Graphic\Transformation.h"
+#include "Graphic/Mesh.h"
+#include "Graphic/Shader.h"
+#include "Graphic/Texture.h"
+#include "Graphic/Transformation.h"
 
 int main(int argc, char* argv[])
 {
